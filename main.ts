8306870f9e54c387673b0aaa0f1223c3b9134afa
augustquinlan <74929759+augustquinlan@users.noreply.github.com>
2020-11-23 13:04:46 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
+    basic.showString("Happy")
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Angry)
+    basic.showString("Mad")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Sad)
+    basic.showString("Sad")
 })
